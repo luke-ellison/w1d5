ccd728757ab4e1eb0b0a2224281ed61f407e7081
App Academy Student <student@appacademy.io>
2017-01-27 16:57:25 -0800

    fix bugs in knight path finder

--- knights-travails/knight-path-finder.rb
+++ knights-travails/knight-path-finder.rb
@@ -1,4 +1,4 @@
-require '../skeleton/lib/00_tree_node.rb'
+require '../poly-tree-node/lib/00_tree_node.rb'
 
 class KnightPathFinder
 
@@ -34,29 +34,23 @@ class KnightPathFinder
   def find_path(end_pos)
     path = []
 
-    curr_node = @visited_positions.bfs(end_pos)
+    curr_node = @visited_positions[0].bfs(end_pos)
     while curr_node.parent
       path << curr_node.value
       curr_node = curr_node.parent
     end
 
-    [@starting_pos] + path.reverse
+    ([@starting_pos] + path.reverse).map { |pos| pos.to_s }.join(" => ")
   end
 
   def build_move_tree
-    @visited_positions = PolyTreeNode.new(@starting_pos)
-    @num_visited = 1
+    @visited_positions = [PolyTreeNode.new(@starting_pos)]
+    i = 0
 
-    curr_nodes = [@visited_positions]
-    until @num_visited == BOARD_SIDE_LENGTH ** 2
-      # curr_node = curr_nodes.unshift
-      # curr_nodes.concat(new_move_positions(curr_node))
-      curr_nodes.each do |node|
-        new_move_positions(node)
-      end
-      new_nodes = []
-      curr_nodes.each { |node| new_nodes.concat(node.children) }
-      curr_nodes = new_nodes
+    until i == BOARD_SIDE_LENGTH ** 2 || i == @visited_positions.length
+      curr_node = @visited_positions[i]
+      @visited_positions.concat(new_move_positions(curr_node))
+      i += 1
     end
 
     @visited_positions
@@ -66,15 +60,13 @@ class KnightPathFinder
     new_nodes = []
 
     KnightPathFinder.valid_moves(node.value).each do |pos|
-      unless @visited_positions.bfs(pos)
+      unless @visited_positions[0].bfs(pos)
         baby_node = PolyTreeNode.new(pos)
         node.add_child(baby_node)
         new_nodes << baby_node
       end
     end
-    @num_visited += new_nodes.length
 
-    # p new_nodes
     new_nodes
   end
 
@@ -83,5 +75,5 @@ end
 if __FILE__ == $0
   k = KnightPathFinder.new([0, 0])
   k.build_move_tree
-  p k.find_path([6, 2])
+  puts k.find_path([6, 2])
 end
